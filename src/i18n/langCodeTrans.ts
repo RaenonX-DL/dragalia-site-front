9f67e7a60c479fc6af2e0f22c80ea95c667d7a7b
RaenonX <raenonx0710@gmail.com>
2020-12-01 12:40:13 -0600

    ADD - JP language support
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/i18n/langCodeTrans.ts
+++ src/i18n/langCodeTrans.ts
@@ -1,10 +1,14 @@
+const LANG_CODE_CHT = 'cht';
+
 const LANG_CODE_EN = 'en';
 
-const LANG_CODE_CHT = 'cht';
+const LANG_CODE_JP = 'jp';
+
+const LANG_CODE_TO_CHT = ['zh'];
 
 const LANG_CODE_TO_EN = ['en', 'es'];
 
-const LANG_CODE_TO_CHT = ['zh'];
+const LANG_CODE_TO_JP = ['jp', 'ja'];
 
 
 export const mapToCustomCode = (orgLang: string | undefined) => {
@@ -20,6 +24,10 @@ export const mapToCustomCode = (orgLang: string | undefined) => {
 
   if (LANG_CODE_TO_EN.some((code) => code === langCode)) {
     return LANG_CODE_EN;
+  }
+
+  if (LANG_CODE_TO_JP.some((code) => code === langCode)) {
+    return LANG_CODE_JP;
   }
 
   return LANG_CODE_CHT;
