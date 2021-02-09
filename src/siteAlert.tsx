be259239b501abae002bf6da3251e2293623f1ae
RaenonX <raenonx0710@gmail.com>
2021-02-08 22:16:53 -0600

    ADD - Hard-coded donation alerts
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ src/siteAlert.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Alert} from 'react-bootstrap';
+import {useTranslation} from 'react-i18next';
+import {Markdown} from './components/elements/markdown/main';
+
+export const SiteAlert = () => {
+  const {t} = useTranslation();
+
+  return (
+    <Alert variant="info"> <Markdown>{t('message.info.donation')}</Markdown></Alert>
+  );
+};
