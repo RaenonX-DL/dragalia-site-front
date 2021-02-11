9aac07229e237373ff3947c3265074cce032f92b
RaenonX <raenonx0710@gmail.com>
2021-02-11 11:18:46 -0600

    ADD - Special thanks
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ src/components/pages/thanks.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Badge} from 'react-bootstrap';
+import {useTranslation} from 'react-i18next';
+import {Markdown} from '../elements/markdown/main';
+import {PageProps} from './base';
+
+export const SpecialThanks = ({fnSetTitle}: PageProps) => {
+  const {t} = useTranslation();
+
+  fnSetTitle(t('pages.name.thanks'));
+
+  return (
+    <>
+      <h3>
+        <a href="https://patreon.com/RaenonX" target="_blank" rel="noreferrer">
+          Patreon
+        </a>
+      </h3>
+      <ul>
+        <li>
+          Andy (au********dy@yahoo.com.tw)&nbsp;
+          <Badge variant="info">{t('donation.tier_s2')}</Badge>&nbsp;
+          <Badge variant="orange">{t('misc.om_member')}</Badge>
+        </li>
+        <li>
+          Ellie (a*****0@yahoo.com.tw)&nbsp;
+          <Badge variant="info">{t('donation.tier_s2')}</Badge>&nbsp;
+          <Badge variant="orange">{t('misc.om_member')}</Badge>
+        </li>
+      </ul>
+      <hr/>
+      <div>{<Markdown>{t('message.info.donation')}</Markdown>}</div>
+    </>
+  );
+};
