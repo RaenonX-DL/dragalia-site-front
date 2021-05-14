5b4090ec15a481e165191b46f8d8b097f6d56eeb
RaenonX <raenonx0710@gmail.com>
2021-05-13 22:10:54 -0500

    FIX - Removed rate limiter (Cloudflare handles that)

--- service/middleware/rateLimit.ts
+++
@@ -1,8 +0,0 @@
-// False positive of `esModuleInterop` not provided
-// @ts-ignore
-import rateLimit from 'express-rate-limit';
-
-export const commonRateLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 20, // 30 for each IP
-});
