706376e98b18dd47a8cf722bd1e317395f68504b
RaenonX <raenonx0710@gmail.com>
2021-05-19 02:30:59 -0500

    FIX - 404 to use react-bootstrap row/col
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/pages/404.tsx
+++ src/components/pages/404.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import {Col, Row} from 'react-bootstrap';
+
 import {useI18n} from '../../i18n/hook';
 
 export const Error404 = () => {
   const {t} = useI18n();
 
   return (
-    <div className="row">
-      <div className="col text-center text-danger">
+    <Row>
+      <Col className="text-center text-danger">
         <h3>{t((t) => t.meta.error['404'].description)}</h3>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 };
