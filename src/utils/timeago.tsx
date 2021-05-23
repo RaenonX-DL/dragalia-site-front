3746617226be6a1db8a3c0fdaa22f630741f3d47
RaenonX <raenonx0710@gmail.com>
2021-05-23 07:31:36 -0500

    IMP - Changed time-ago translations for CHT
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/timeago.tsx
+++ src/utils/timeago.tsx
@@ -8,11 +8,28 @@ import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
 import enStrings from 'react-timeago/lib/language-strings/en';
 // @ts-ignore
 import jpStrings from 'react-timeago/lib/language-strings/ja';
-// @ts-ignore
-import chtStrings from 'react-timeago/lib/language-strings/zh-TW';
 
 import {SupportedLanguages} from '../api-def/api/other/lang';
 import {useI18n} from '../i18n/hook';
+
+const chtStrings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: '前',
+  suffixFromNow: '後',
+  seconds: '<1 分',
+  minute: '約 1 分',
+  minutes: '%d 分',
+  hour: '約 1 小時',
+  hours: '%d 小時',
+  day: '約 1 天',
+  days: '%d 天',
+  month: '約 1 個月',
+  months: '%d 個月',
+  year: '約 1 年',
+  years: '%d 年',
+  wordSeparator: '',
+};
 
 const formatters: { [lang in SupportedLanguages]: Formatter } = {
   [SupportedLanguages.EN]: buildFormatter(enStrings),
