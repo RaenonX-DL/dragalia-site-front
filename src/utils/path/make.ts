bd792439b1eba31cea893e57821566dcd7fe73ca
RaenonX <raenonx0710@gmail.com>
2021-06-05 14:05:55 -0500

    IMP - Remove unused path maker
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/path/make.ts
+++ src/utils/path/make.ts
@@ -1,20 +1,9 @@
-import {SupportedLanguages} from '../../api-def/api';
 import {PostPath} from '../../const/path/definitions';
-
-// FIXME: Check usages (makePat should not be necessary)
 
 const generatePath = (path: string, args: { [key in string]: string | number }) => {
   Object.keys(args).forEach((key) => path = path.replace(`:${key}`, args[key].toString()));
 
   return path;
-};
-
-export const makeLangSensitivePath = (path: string, lang: SupportedLanguages) => {
-  if (!path.startsWith('/')) {
-    path = '/' + path;
-  }
-
-  return `/${lang}${path}`;
 };
 
 type PostPathArgs = {
