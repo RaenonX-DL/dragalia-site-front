d82bea6881c765c1cdb66de7530325700dcf3cb2
RaenonX <raenonx0710@gmail.com>
2021-06-14 21:43:44 -0500

    FIX - Temporarily remove ads visibility tests
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {Session} from 'next-auth';
-import {getSession, Provider} from 'next-auth/client';
+import {getSession} from 'next-auth/client';
 import App, {AppProps, AppContext, AppInitialProps as NextAppInitialProps} from 'next/app';
 import Head from 'next/head';
 import {Container} from 'react-bootstrap';
@@ -34,8 +34,6 @@ type AppInitialProps = NextAppInitialProps & {
 const NextApp = ({Component, pageProps}: AppProps<PageProps>) => {
   const {t} = useI18n();
 
-  console.log('SESSION IN APP', pageProps);
-
   // Page meta must be obtained here, or page preview won't work
   return (
     <>
@@ -45,28 +43,27 @@ const NextApp = ({Component, pageProps}: AppProps<PageProps>) => {
 
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-        {/* Google AdSense - ads showing or not to be determined by the component */}
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>
+        {/* Google AdSense */}
+        {
+          !pageProps.session?.user.adsFreeExpiry &&
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>
+        }
       </Head>
-      {/* Boost the performance as suggested, also bugs possible if not wrapped */}
-      {/* https://github.com/nextauthjs/next-auth/discussions/704#discussioncomment-154252 */}
-      <Provider session={pageProps.session}>
-        <AppReactContext.Provider value={{...pageProps}}>
-          <ReduxProvider>
-            <Navigation/>
-            <SiteAlert/>
-            {
-              pageProps.isNotFound ?
-                <Error404/> :
-                <Container className="p-3">
-                  <GlobalAlert/>
-                  <Component {...pageProps} />
-                </Container>
-            }
-            <Footer/>
-          </ReduxProvider>
-        </AppReactContext.Provider>
-      </Provider>
+      <AppReactContext.Provider value={{...pageProps}}>
+        <ReduxProvider>
+          <Navigation/>
+          <SiteAlert/>
+          {
+            pageProps.isNotFound ?
+              <Error404/> :
+              <Container className="p-3">
+                <GlobalAlert/>
+                <Component {...pageProps} />
+              </Container>
+          }
+          <Footer/>
+        </ReduxProvider>
+      </AppReactContext.Provider>
     </>
   );
 };
@@ -74,10 +71,7 @@ const NextApp = ({Component, pageProps}: AppProps<PageProps>) => {
 NextApp.getInitialProps = async (appContext: AppContext): Promise<AppInitialProps> => {
   // Call page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext) as AppInitialProps;
-
   const session = await getSession(appContext.ctx);
-
-  console.log('SESSION', session);
 
   appProps.pageProps = {
     ...await getPageMeta(appContext),
