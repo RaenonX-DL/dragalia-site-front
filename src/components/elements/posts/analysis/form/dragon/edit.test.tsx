ed90dd908b9902951ceae594827f51e0d5f9d9ed
RaenonX <raenonx0710@gmail.com>
2021-06-22 14:57:42 -0500

    ADD - Tests for form edit note attachment
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/posts/analysis/form/dragon/edit.test.tsx
+++ src/components/elements/posts/analysis/form/dragon/edit.test.tsx
@@ -1,3 +1,67 @@
+import React from 'react';
+
+import {screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import {renderReact} from '../../../../../../../test/render/main';
+import {ApiResponseCode, SupportedLanguages, UnitType} from '../../../../../../api-def/api';
+import {translation as translationEN} from '../../../../../../i18n/translations/en/translation';
+import {ApiRequestSender} from '../../../../../../utils/services/api/requestSender';
+import * as unitInfoUtils from '../../../../../../utils/services/resources/unitInfo/utils';
+import {AnalysisFormDragonEdit} from './edit';
+
+
 describe('Dragon analysis edit form', () => {
-  it.todo('attaches edit note');
+  let fnSendRequest: jest.SpyInstance;
+
+  beforeEach(() => {
+    jest.spyOn(ApiRequestSender, 'analysisIdCheck').mockResolvedValue({
+      code: ApiResponseCode.SUCCESS,
+      success: true,
+      available: true,
+    });
+    // Mocking this because the fetching promises in `getUnitNameIdMap()` do not resolve
+    jest.spyOn(unitInfoUtils, 'getUnitNameIdMap').mockResolvedValue(new Map([
+      ['Some Dragon', 20950101],
+    ]));
+    fnSendRequest = jest.spyOn(ApiRequestSender, 'analysisEditDragon').mockResolvedValue({
+      code: ApiResponseCode.SUCCESS,
+      success: true,
+      unitId: 20950101,
+    });
+  });
+
+  it('attaches edit note', async () => {
+    renderReact(
+      () => (
+        <AnalysisFormDragonEdit
+          analysis={{
+            unitId: 20950101,
+            lang: SupportedLanguages.EN,
+            type: UnitType.DRAGON,
+            summary: 'summary',
+            summonResult: 'smn',
+            passives: 'psv',
+            normalAttacks: 'nrm',
+            ultimate: 'ult',
+            suitableCharacters: 'sut',
+            notes: 'not',
+            videos: 'vid',
+            story: 'str',
+            keywords: 'kw',
+          }}
+        />
+      ),
+      {user: {isAdmin: true}},
+    );
+
+    const editNote = screen.getByPlaceholderText(translationEN.posts.manage.editNote);
+    userEvent.type(editNote, 'Edit Note');
+
+    const editButton = screen.getByText(translationEN.posts.manage.edit);
+    userEvent.click(editButton);
+
+    await waitFor(() => expect(fnSendRequest).toHaveBeenCalled());
+    expect(fnSendRequest.mock.calls[0][0].editNote).toBe('Edit Note');
+  });
 });
