2e8228c52dc6cf4719bacf1a7ebee648cba660d7
RaenonX <raenonx0710@gmail.com>
2021-06-22 00:28:41 -0500

    ADD - Test for transforming empty string
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/process/text.test.ts
+++ src/utils/process/text.test.ts
@@ -2,9 +2,17 @@ import {SupportedLanguages} from '../../api-def/api';
 import {PostPath} from '../../const/path/definitions';
 import {translations} from '../../i18n/translations/main';
 import {makePostPath} from '../path/make';
+import * as unitInfoUtils from '../services/resources/unitInfo/utils';
 import {processText} from './text';
 
 describe('Process text', () => {
+  beforeEach(() => {
+    // Mocking this because the fetching promises in `getUnitNameIdMap()` do not resolve
+    jest.spyOn(unitInfoUtils, 'getUnitNameIdMap').mockResolvedValue(new Map([
+      ['Gala Mym', 10550101],
+    ]));
+  });
+
   it('transforms all quick references at once', async () => {
     const text = 'Miscellaneous post #M3 Quest Post #Q1 Gala Mym Analysis';
     const lang = SupportedLanguages.EN;
@@ -21,4 +29,15 @@ describe('Process text', () => {
       `[Gala Mym](${makePostPath(PostPath.ANALYSIS, {pid: 10550101, lang})}) Analysis`;
     expect(result).toBe(`${expectedMisc} ${expectedQuest} ${expectedAnalysis}`);
   });
+
+  it('transforms empty string without error', async () => {
+    const text = '';
+    const lang = SupportedLanguages.EN;
+
+    const result = await processText({text, lang});
+
+    expect(result).toBe('');
+  });
+
+  it.todo('keeps already transformed references intact');
 });
