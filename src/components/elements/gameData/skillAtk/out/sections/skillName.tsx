1b0d7cdb744cc681902dee6ec3c4ddb7337e3c34
RaenonX <raenonx0710@gmail.com>
2021-06-26 01:15:32 -0500

    ADD - ATK skill entry show unit name (#155)
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/gameData/skillAtk/out/sections/skillName.tsx
+++ src/components/elements/gameData/skillAtk/out/sections/skillName.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import {SkillIdentifierInfo} from '../../../../../../api-def/resources';
+import {PostPath} from '../../../../../../const/path/definitions';
 import {useI18n} from '../../../../../../i18n/hook';
+import {makePostPath} from '../../../../../../utils/path/make';
+import {NextLink} from '../../../../common/link';
 import {SectionProps} from '../props';
 
 
@@ -14,15 +17,18 @@ export const SectionSkillName = ({atkSkillEntry, skillIdentifierInfo}: SectionSk
 
   return (
     <>
-      <span className="h5">
+      <div className="h5 mb-1">
+        <NextLink href={makePostPath(PostPath.ANALYSIS, {pid: atkSkillEntry.chara.id, lang})}>
+          {atkSkillEntry.chara.name[lang]}
+        </NextLink>
+      </div>
+      <div className="text-white-50">
         {
           atkSkillEntry.skill.identifiers
             .map((identifier) => skillIdentifierInfo[identifier].trans[lang])
             .join(' / ')
         }
-      </span>
-      <br/>
-      <small>{atkSkillEntry.skill.name[lang]}</small>
+      </div>
     </>
   );
 };
