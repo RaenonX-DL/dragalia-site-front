979361f4ffcf3d9b740d242f8ab76c413742366b
RaenonX <raenonx0710@gmail.com>
2021-06-27 02:09:42 -0500

    IMP - Co-ab lookup input to use input panel
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/gameData/ex/in/filter.tsx
+++ src/components/elements/gameData/ex/in/filter.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import {ElementEnums, ExBuffParams} from '../../../../../api-def/resources';
-import {useI18n} from '../../../../../i18n/hook';
-import {EnumCheckboxGroup} from '../../../common/check/enum/checkbox';
-import {SectionTitle} from '../../elements/title';
+import {InputPanel} from '../../../input/main';
 import {InputSectionBaseProps} from '../../props';
 import {InputData} from './types';
 import {overwriteInputData} from './utils';
@@ -20,45 +18,47 @@ export const SectionFilter = ({
   elementEnums,
   exBuffParams,
 }: SectionProps) => {
-  const {t} = useI18n();
-
   return (
-    <>
-      <SectionTitle
-        title={t((t) => t.game.ex.name.filterElement)}
-        description={t((t) => t.game.ex.desc.filterElement)}
-      />
-      <EnumCheckboxGroup
-        options={elementEnums.elemental}
-        inputData={inputData}
-        setInputData={setInputData}
-        getValue={(inputData) => inputData.filter.elements}
-        getUpdatedInputData={(newValue) => overwriteInputData(inputData, {filter: {elements: newValue}})}
-      />
-      <SectionTitle
-        title={t((t) => t.game.ex.name.filterExBuffParam)}
-        description={t((t) => t.game.ex.desc.filterExBuffParam)}
-      />
-      <EnumCheckboxGroup
-        options={exBuffParams.exBuffParam}
-        inputData={inputData}
-        setInputData={setInputData}
-        getValue={(inputData) => inputData.filter.exBuffParams}
-        getUpdatedInputData={(newValue) => overwriteInputData(inputData, {filter: {exBuffParams: newValue}})}
-        imageHeight="2rem"
-      />
-      <SectionTitle
-        title={t((t) => t.game.ex.name.filterChainedExBuffParam)}
-        description={t((t) => t.game.ex.desc.filterChainedExBuffParam)}
-      />
-      <EnumCheckboxGroup
-        options={exBuffParams.chainedExBuffParam}
-        inputData={inputData}
-        setInputData={setInputData}
-        getValue={(inputData) => inputData.filter.cexBuffParams}
-        getUpdatedInputData={(newValue) => overwriteInputData(inputData, {filter: {cexBuffParams: newValue}})}
-        imageHeight="2rem"
-      />
-    </>
+    <InputPanel
+      inputData={inputData}
+      setInputData={setInputData}
+      inputEntries={[
+        {
+          type: 'title',
+          title: (t) => t.game.ex.name.filterElement,
+          description: (t) => t.game.ex.desc.filterElement,
+        },
+        {
+          type: 'enumCheckGroup',
+          options: elementEnums.elemental,
+          getValue: (inputData) => inputData.filter.elements,
+          getUpdatedInputData: (newValue) => overwriteInputData(inputData, {filter: {elements: newValue}}),
+        },
+        {
+          type: 'title',
+          title: (t) => t.game.ex.name.filterExBuffParam,
+          description: (t) => t.game.ex.desc.filterExBuffParam,
+        },
+        {
+          type: 'enumCheckGroup',
+          options: exBuffParams.exBuffParam,
+          getValue: (inputData) => inputData.filter.exBuffParams,
+          getUpdatedInputData: (newValue) => overwriteInputData(inputData, {filter: {exBuffParams: newValue}}),
+          imageHeight: '2rem',
+        },
+        {
+          type: 'title',
+          title: (t) => t.game.ex.name.filterChainedExBuffParam,
+          description: (t) => t.game.ex.desc.filterChainedExBuffParam,
+        },
+        {
+          type: 'enumCheckGroup',
+          options: exBuffParams.chainedExBuffParam,
+          getValue: (inputData) => inputData.filter.cexBuffParams,
+          getUpdatedInputData: (newValue) => overwriteInputData(inputData, {filter: {cexBuffParams: newValue}}),
+          imageHeight: '2rem',
+        },
+      ]}
+    />
   );
 };
