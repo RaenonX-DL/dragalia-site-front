a428df99a8a27f9278280bd345d7324888b9cbc9
RaenonX <raenonx0710@gmail.com>
2021-06-29 19:39:26 -0500

    IMP - ATK skill to filter outliers
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/gameData/skillAtk/sorter/lookup.ts
+++ src/components/elements/gameData/skillAtk/sorter/lookup.ts
@@ -4,23 +4,32 @@ import {SortBy} from '../in/types';
 import {CalculatedSkillEntry} from '../out/types';
 
 
+// Unique dragon skills = 30 SP, Tobias S2 = 1 SP
+const isSkillNotCommonSp = (entry: CalculatedSkillEntry) => entry.skillEntry.skill.spMax < 100;
+
 type SortFuncLookup = { [sortBy in SortBy]: (entryA: CalculatedSkillEntry, entryB: CalculatedSkillEntry) => number }
 
 export const sortFunc: SortFuncLookup = {
   damage: sortDescending({getComparer: (element) => element.skillDamage.expected}),
-  sp: sortAscending({getComparer: (element) => element.skillEntry.skill.spMax}),
+  sp: sortAscending({
+    getComparer: (element) => element.skillEntry.skill.spMax,
+    isToPutLast: isSkillNotCommonSp,
+  }),
   ssp: sortAscending({
     getComparer: (element) => element.skillEntry.skill.ssSp,
     isToPutLast: (element) => !element.skillEntry.skill.sharable,
   }),
-  modPer1KSp: sortDescending({getComparer: (element) => element.efficiency.modPctPer1KSp}),
+  modPer1KSp: sortDescending({
+    getComparer: (element) => element.efficiency.modPctPer1KSp,
+    isToPutLast: isSkillNotCommonSp,
+  }),
   modPer1KSsp: sortDescending({
     getComparer: (element) => element.efficiency.modPctPer1KSsp,
     isToPutLast: (element) => !element.skillEntry.skill.sharable,
   }),
   afflictionLengthPer1KSp: sortDescending({
     getComparer: (element) => Math.min(...Object.values(element.efficiency.secPer1KSp)),
-    isToPutLast: (element) => !element.skillEntry.skill.afflictions.length,
+    isToPutLast: (element) => !element.skillEntry.skill.afflictions.length || isSkillNotCommonSp(element),
   }),
   afflictionLengthPer1KSsp: sortDescending({
     getComparer: (element) => Math.min(...Object.values(element.efficiency.secPer1KSsp)),
