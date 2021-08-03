d5e2232dbad46601bab4f741ba362dd54031cc54
RaenonX <raenonx0710@gmail.com>
2021-08-02 20:22:39 -0500

    ADD - Utils to check failed API response
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/meta/main.ts
+++ src/utils/meta/main.ts
@@ -1,5 +1,6 @@
 import {AppContext} from 'next/app';
 
+import {isFailedResponse} from '../../api-def/api';
 import {isPagePath} from '../../const/path/utils';
 import {PageMetaTranslations} from '../../i18n/translations/definition';
 import {translations} from '../../i18n/translations/main';
@@ -9,7 +10,7 @@ import {getLangFromQuery, pathnameRemoveLang} from '../path/process';
 import {onMetaResponseFailed, onNotFound} from './handler';
 import {getPageMetaPromise} from './preprocess';
 import {metaTransFunctions} from './translations';
-import {PageMeta, isMetaResponseFailure} from './types';
+import {PageMeta} from './types';
 
 
 export const getPageMeta = async (context: AppContext): Promise<PageMeta> => {
@@ -31,7 +32,7 @@ export const getPageMeta = async (context: AppContext): Promise<PageMeta> => {
 
   const metaResponse = await getPageMetaPromise({lang, pathnameNoLang, context});
 
-  if (isMetaResponseFailure(metaResponse)) {
+  if (isFailedResponse(metaResponse)) {
     return {
       ...onMetaResponseFailed(context, metaResponse, metaTFunc(metaTFuncOnNotFound)),
       alerts: [],
