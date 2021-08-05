de2337311c467699cb9d839fbf91dd0d585ae53e
RaenonX <raenonx0710@gmail.com>
2021-08-05 15:56:12 -0500

    FIX - Failing post manage bar tests
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/posts/manageBar.test.tsx
+++ src/components/elements/posts/manageBar.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {screen} from '@testing-library/react';
 
 import {renderReact} from '../../../../test/render/main';
+import {SupportedLanguages} from '../../../api-def/api';
 import {GeneralPath} from '../../../const/path/definitions';
 import {translation as translationEN} from '../../../i18n/translations/en/translation';
 import {PostManageBar} from './manageBar';
@@ -17,24 +18,25 @@ describe('Post manage bar', () => {
           text: 'New',
         },
         {
-          pathname: GeneralPath.HOME,
+          pathname: GeneralPath.EX,
           text: 'New 2',
         },
       ]}/>
     ));
 
-    expect(screen.getByText('New')).toHaveAttribute('href', GeneralPath.ABOUT);
-    expect(screen.getByText('New 2')).toHaveAttribute('href', GeneralPath.HOME);
+    expect(screen.getByText('New')).toHaveAttribute('href', `/${SupportedLanguages.EN}${GeneralPath.ABOUT}`);
+    expect(screen.getByText('New 2')).toHaveAttribute('href', `/${SupportedLanguages.EN}${GeneralPath.EX}`);
   });
 
   it('has `href` to edit a post', async () => {
     renderReact(() => (
       <PostManageBar
         newButtons={[{pathname: GeneralPath.ABOUT, text: 'New'}]}
-        editPostUrl={'/edit'}
+        editPostUrl="/edit"
       />
     ));
 
-    expect(screen.getByText(translationEN.posts.manage.edit)).toHaveAttribute('href', '/edit');
+    const expectedLink = `/${SupportedLanguages.EN}/edit`;
+    expect(screen.getByText(translationEN.posts.manage.edit)).toHaveAttribute('href', expectedLink);
   });
 });
