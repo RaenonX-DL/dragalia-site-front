0274a2b8b2800b78f2ba8c36697252d3ff1c4c26
RaenonX <raenonx0710@gmail.com>
2021-08-20 14:34:50 -0500

    OTH - Changed default sort order to avg ranking for tier note
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/pages/tier/utils.ts
+++ src/components/pages/tier/utils.ts
@@ -3,7 +3,7 @@ import {generateFilterInput} from '../../elements/gameData/unit/filter/utils';
 import {InputData} from './types';
 
 
-export const generateInputData = (): InputData => generateFilterInput('unitId');
+export const generateInputData = (): InputData => generateFilterInput('avgRanking');
 
 export const getTierRanking = (tierNote: UnitTierNote, tierDimension: DimensionKey) => {
   const tierNoteDimension = tierNote.tier[tierDimension];
