606f3bc328ae9afff36aab80859665603344fe82
RaenonX <raenonx0710@gmail.com>
2021-08-20 17:36:17 -0500

    ADD - Key points index page (#258)
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/pages/tier/points/index/main.tsx
+++ src/components/pages/tier/points/index/main.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import {DataPath} from '../../../../../const/path/definitions';
+import {useI18n} from '../../../../../i18n/hook';
+import {makeDataUrl} from '../../../../../utils/path/make';
 import {Loading} from '../../../../elements/common/loading';
+import {Search} from '../../../../elements/input/search/main';
 import {useKeyPointData} from '../../hooks';
+import {PointTypeIcon} from '../../icons';
+import styles from './main.module.css';
 
 
 export const KeyPointIndexPage = () => {
-  const {isFetched} = useKeyPointData();
+  const {t, lang} = useI18n();
+  const {keyPointData, isFetched} = useKeyPointData();
 
   if (!isFetched) {
     return <Loading/>;
   }
 
-  return <>WIP</>;
+  return (
+    <Search
+      options={Object.entries(keyPointData).map(([id, entry]) => ({id, ...entry}))}
+      isOptionMatchSearch={(option, textLowered) => option.description.toLowerCase().includes(textLowered)}
+      renderMatchedSelection={({id, type, description}) => (
+        <Row noGutters key={description} className={styles.entry}>
+          <Col xs="auto" className={styles.typeEntry}>
+            <span className={styles.typeIcon}>{PointTypeIcon[type]}</span>&nbsp;
+            <small>{t((t) => t.game.unitTier.points.type[type])}</small>
+          </Col>
+          <Col>
+            <a href={makeDataUrl(DataPath.TIER_KEY_POINT, {id, lang})} target="_blank" rel="noreferrer">
+              {description}
+            </a>
+          </Col>
+        </Row>
+      )}
+      height="70vh"
+    />
+  );
 };
