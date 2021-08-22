8df9b207749871d060154c4b38d7266abdb859ff
RaenonX <raenonx0710@gmail.com>
2021-08-22 08:00:57 -0500

    ADD - Ads test border
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/common/ads/wrapper.tsx
+++ src/components/elements/common/ads/wrapper.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import {isProduction} from '../../../../../server/utils/misc';
 import {AppReactContext} from '../../../../context/app/main';
+import styles from './main.module.css';
 
 
 type Props = React.PropsWithChildren<{
@@ -10,7 +12,7 @@ type Props = React.PropsWithChildren<{
 export const AdsWrapper = ({children, className}: Props) => {
   const context = React.useContext(AppReactContext);
 
-  const divClassName = className ?? 'mb-3';
+  const divClassName = `${className ?? 'mb-3'} ${isProduction() ? '' : styles.adsTest}`;
 
   if (context?.session?.user.adsFreeExpiry) {
     return <div className={divClassName}/>;
