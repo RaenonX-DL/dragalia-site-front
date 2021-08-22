80b68d2faaf0e3777313414eb8249767b4845a48
RaenonX <raenonx0710@gmail.com>
2021-08-22 02:33:13 -0500

    IMP - Refactor note icon
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/pages/tier/out/elements/ranking.tsx
+++ src/components/pages/tier/out/elements/ranking.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 import {TierNote} from '../../../../../api-def/api';
-import {IconNotes} from '../../../../elements/common/icons';
-import {ModalFixedContent} from '../../../../elements/common/modal/fix';
-import {ModalStateFix} from '../../../../elements/common/modal/types';
-import {Markdown} from '../../../../elements/markdown/main';
 import {rankingColor} from '../../const';
 import {IconCompDependent} from '../../icons';
 import styles from '../../main.module.css';
+import {TierNoteIcon} from './noteIcon';
 
 
 export type RankingProps = {
@@ -15,29 +12,17 @@ export type RankingProps = {
 }
 
 export const TierRanking = ({tierNote}: RankingProps) => {
-  const [modalState, setModalState] = React.useState<ModalStateFix>({
-    show: false,
-    title: '',
-  });
-
   if (!tierNote) {
     return <span className={styles.ranking}>-</span>;
   }
 
   return (
     <>
-      <ModalFixedContent state={modalState} setState={setModalState}>
-        <Markdown overrideStyle={false}>
-          {tierNote?.note}
-        </Markdown>
-      </ModalFixedContent>
       <div className="d-inline">
         <span className={styles.ranking} style={{color: rankingColor[tierNote.ranking]}}>{tierNote.ranking}</span>
-        {tierNote && tierNote.isCompDependent && <>&nbsp;<IconCompDependent/></>}
+        {tierNote.isCompDependent && <>&nbsp;<IconCompDependent/></>}
       </div>
-      <a className={styles.tierNote} onClick={() => setModalState({...modalState, show: true})}>
-        <IconNotes/>
-      </a>
+      <TierNoteIcon tierNote={tierNote} iconClassName={styles.tierNote}/>
     </>
   );
 };
