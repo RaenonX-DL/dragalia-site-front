bdb3d20902f522945db7483179227368dad3ddc5
RaenonX <raenonx0710@gmail.com>
2021-08-22 08:39:38 -0500

    IMP - Matched content ad type to set content size
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/common/ads/unit/matched.tsx
+++ src/components/elements/common/ads/unit/matched.tsx
@@ -10,6 +10,8 @@ import {AdsUnitProps} from './types';
 type Props = AdsUnitProps
 
 export const AdsUnitMatchedContent = ({slot, testId}: Props) => (
+  // About customization: https://support.google.com/adsense/answer/7533385?hl=en&ref_topic=9183242
+  // This renders 1x4 in desktop, 2x2 in mobile
   <AdsWrapper>
     <Adsense
       client={AdsClientId}
@@ -17,6 +19,9 @@ export const AdsUnitMatchedContent = ({slot, testId}: Props) => (
       style={{display: 'block'}}
       format="autorelaxed"
       data-testid={testId}
+      data-matched-content-rows-num="2,1"
+      data-matched-content-columns-num="2,4"
+      data-matched-content-ui-type="image_stacked,image_stacked"
     />
   </AdsWrapper>
 );
