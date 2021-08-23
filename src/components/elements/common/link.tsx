87b3c9f3efcc7aa6c3503834f8d5752d9bd5aa91
RaenonX <raenonx0710@gmail.com>
2021-08-23 14:31:37 -0500

    ADD - `<Link>` element to accept style if using `<a>`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/common/link.tsx
+++ src/components/elements/common/link.tsx
@@ -16,11 +16,12 @@ type Props<T, A extends HTMLAnchorElement> = {
   locale?: never,
   onClick: () => void,
 }) & ({
-  // If `passHref`, children must be an element accepting `href`
+  // If `passHref`, children must be an element accepting `href` - `children` is used
   passHref: true,
   children: T extends React.ReactElement<{href: string, className: string}> ? T : never,
   content?: never,
   anchorProps?: never,
+  style?: never,
 } | {
   // If not `passHref`, children must not exist and `text` must be provided - `<a>` is used
   // - Setting `children` gives false-negative type checking result,
@@ -29,19 +30,20 @@ type Props<T, A extends HTMLAnchorElement> = {
   children?: never,
   content: React.ReactNode,
   anchorProps?: React.AnchorHTMLAttributes<A>,
+  style?: React.CSSProperties,
 })
 
 export const Link = <T, A extends HTMLAnchorElement>({
-  locale, href, onClick, className, passHref, children, content, anchorProps,
+  className, style, locale, href, onClick, passHref, children, content, anchorProps,
 }: Props<T, A>) => {
   if (!href) {
-    return <a className={className} onClick={onClick}>{content}</a>;
+    return <a style={style} className={className} onClick={onClick}>{content}</a>;
   }
 
   href = `/${locale}${urlRemoveLang(href)}`;
 
   if (!passHref || !children) {
-    return <a className={className} href={href} {...anchorProps}>{content}</a>;
+    return <a style={style} className={className} href={href} {...anchorProps}>{content}</a>;
   }
 
   return <>{React.cloneElement(children, {href, className})}</>;
