1afd3c1b1829af16b4795a63e681b6a9fe78d63f
RaenonX <raenonx0710@gmail.com>
2021-09-10 03:48:40 -0500

    IMP - 404 page style
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 import {useI18n} from '../src/i18n/hook';
@@ -11,13 +10,11 @@ const Error404 = () => {
   const {t} = useI18n();
 
   return (
-    <Container className="p-3">
-      <Row>
-        <Col className="text-center text-danger">
-          <span className="h2">{t((t) => t.meta.error['404'].description)}</span>
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col className="text-center text-danger">
+        <span className="h2">{t((t) => t.meta.error['404'].description)}</span>
+      </Col>
+    </Row>
   );
 };
 
