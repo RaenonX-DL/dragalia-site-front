ee09aa922719118ce46492ac6c97a0066f8b32bf
RaenonX <raenonx0710@gmail.com>
2021-09-10 05:41:13 -0500

    ADD - Misc post related page tests
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ server/tests/misc/get.test.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import {screen} from '@testing-library/react';
+
+import {
+  ApiResponseCode, MiscPostGetResponse,
+  SupportedLanguages,
+} from '../../../src/api-def/api';
+import {MiscPage} from '../../../src/components/pages/posts/misc/output';
+import {DEFAULT_LANG} from '../../../src/i18n/langCode';
+import {translations} from '../../../src/i18n/translations/main';
+import {ApiRequestSender} from '../../../src/utils/services/api/requestSender';
+import {renderReact} from '../../../test/render/main';
+
+
+describe('Misc page', () => {
+  const description401 = translations[SupportedLanguages.EN].meta.error['401'].description;
+  const description404 = translations[SupportedLanguages.EN].meta.error['404'].description;
+
+  let fnFetch: jest.SpyInstance;
+
+  const response: MiscPostGetResponse = {
+    code: ApiResponseCode.SUCCESS,
+    success: false,
+    lang: DEFAULT_LANG,
+    isAltLang: false,
+    otherLangs: [],
+    seqId: 0,
+    title: 'ttl',
+    sections: [
+      {title: 'title', content: 'content'},
+    ],
+    viewCount: 0,
+    editNotes: [],
+    publishedEpoch: 55,
+    modifiedEpoch: 55,
+  };
+
+  beforeEach(() => {
+    fnFetch = jest.spyOn(ApiRequestSender, 'miscGet').mockResolvedValue(response);
+  });
+
+  it('allows access for anonymous users', async () => {
+    renderReact(() => <MiscPage/>, {hasSession: false});
+
+    expect(await screen.findByText(response.sections[0].content)).toBeInTheDocument();
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+  });
+
+  it('allows access for non-admin users', async () => {
+    renderReact(() => <MiscPage/>, {user: {isAdmin: false}});
+
+    expect(await screen.findByText(response.sections[0].content)).toBeInTheDocument();
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+  });
+
+  it('allows access for admin users', async () => {
+    renderReact(() => <MiscPage/>, {user: {isAdmin: true}});
+
+    expect(await screen.findByText(response.sections[0].content)).toBeInTheDocument();
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+  });
+
+  it('returns 404 if the post is not found', async () => {
+    fnFetch.mockResolvedValueOnce({
+      code: ApiResponseCode.FAILED_POST_NOT_EXISTS,
+      success: false,
+    });
+
+    renderReact(() => <MiscPage/>);
+
+    expect(await screen.findByText(description404)).toBeInTheDocument();
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(response.sections[0].content)).not.toBeInTheDocument();
+  });
+
+  it('shows at least 2 ads', async () => {
+    renderReact(() => <MiscPage/>, {user: {isAdmin: true}});
+
+    expect((await screen.findAllByTestId('ads-in-post')).length).toBeGreaterThanOrEqual(2);
+  });
+
+  it('does not show ads if should not show', async () => {
+    renderReact(() => <MiscPage/>, {user: {adsFreeExpiry: new Date()}});
+
+    expect(await screen.findByText(response.sections[0].content)).toBeInTheDocument();
+    expect(screen.queryByTestId('ads-in-post')).not.toBeInTheDocument();
+  });
+});
---
+++ server/tests/misc/list.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import {screen} from '@testing-library/react';
+
+import {
+  ApiResponseCode, MiscPostListResponse,
+  SupportedLanguages,
+} from '../../../src/api-def/api';
+import {MiscPostList} from '../../../src/components/pages/posts/misc/list/list';
+import {translations} from '../../../src/i18n/translations/main';
+import {ApiRequestSender} from '../../../src/utils/services/api/requestSender';
+import {renderReact} from '../../../test/render/main';
+
+
+describe('Misc listing page', () => {
+  const description401 = translations[SupportedLanguages.EN].meta.error['401'].description;
+  const description404 = translations[SupportedLanguages.EN].meta.error['404'].description;
+
+  let fnFetchList: jest.SpyInstance;
+
+  const postListResponse: MiscPostListResponse = {
+    code: ApiResponseCode.SUCCESS,
+    success: true,
+    startIdx: 0,
+    postCount: 0,
+    posts: [],
+  };
+
+  beforeEach(() => {
+    fnFetchList = jest.spyOn(ApiRequestSender, 'miscList') .mockResolvedValue(postListResponse);
+  });
+
+  it('allows access for anonymous users', () => {
+    renderReact(() => <MiscPostList/>, {hasSession: false});
+
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+    expect(fnFetchList).toHaveBeenCalledTimes(1);
+  });
+
+  it('allows access for non-admin users', () => {
+    renderReact(() => <MiscPostList/>, {user: {isAdmin: false}});
+
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+    expect(fnFetchList).toHaveBeenCalledTimes(1);
+  });
+
+  it('allows access for admin users', () => {
+    renderReact(() => <MiscPostList/>, {user: {isAdmin: true}});
+
+    expect(screen.queryByText(description401)).not.toBeInTheDocument();
+    expect(screen.queryByText(description404)).not.toBeInTheDocument();
+    expect(fnFetchList).toHaveBeenCalledTimes(1);
+  });
+
+  it('shows at least 3 ads', () => {
+    renderReact(() => <MiscPostList/>, {user: {isAdmin: true}});
+
+    expect(screen.queryAllByTestId('ads-post-list').length).toBeGreaterThanOrEqual(1);
+    expect(fnFetchList).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not show ads if should not show', () => {
+    renderReact(() => <MiscPostList/>, {user: {adsFreeExpiry: new Date()}});
+
+    expect(screen.queryByTestId('ads-post-list')).not.toBeInTheDocument();
+    expect(fnFetchList).toHaveBeenCalledTimes(1);
+  });
+});
