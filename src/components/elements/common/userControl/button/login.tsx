a780c362ae756f04c8a7892f6a816e9a9039a77b
RaenonX <raenonx0710@gmail.com>
2022-01-05 18:58:07 -0600

    Improved sign-in button implementation
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/common/userControl/button/login.tsx
+++ src/components/elements/common/userControl/button/login.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import {useRouter} from 'next/router';
 import Button from 'react-bootstrap/Button';
 
 import {AuthPath} from '../../../../../const/path/definitions';
@@ -9,22 +8,9 @@ import {useI18n} from '../../../../../i18n/hook';
 
 export const LoginButton = () => {
   const {t} = useI18n();
-  // Don't use `useNextRouter()` because `push` from such hook prepends `lang`
-  const {asPath, push} = useRouter();
-
-  const onClick = async () => {
-    // `signIn()` from `next-auth` seems incorrectly redirecting the users
-    // Also, it does not push history
-    await push({
-      pathname: AuthPath.SIGN_IN,
-      query: {
-        callbackUrl: asPath,
-      },
-    });
-  };
 
   return (
-    <Button variant="outline-success" onClick={onClick}>
+    <Button variant="outline-success" href={AuthPath.SIGN_IN}>
       {t((t) => t.userControl.login)}
     </Button>
   );
