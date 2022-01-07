704aafc5f509739093c61025bce025ef0b3f99d7
RaenonX <raenonx0710@gmail.com>
2022-01-06 18:55:30 -0600

    Updated post entry to accept icon
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/elements/posts/list/entry.tsx
+++ src/components/elements/posts/list/entry.tsx
@@ -17,6 +17,7 @@ type Props<E extends PostInfo> = {
   link: string,
   title: string,
   renderPostBadge: FunctionRenderPostBadge<E>,
+  icon?: React.ReactElement,
 };
 
 export const PostEntry = <E extends PostInfo>({
@@ -24,34 +25,40 @@ export const PostEntry = <E extends PostInfo>({
   link,
   title,
   renderPostBadge,
+  icon,
 }: Props<E>) => {
   const {t, lang} = useI18n();
 
   return (
     <div className={styles.entry}>
-      <h5>
-        <InternalLink
-          href={link}
-          locale={lang}
-          content={title}
-        />
-      </h5>
       <Row noGutters>
-        <Col xs="auto">
-          {renderPostBadge({entry})}&nbsp;
-        </Col>
+        {icon ? <Col xs="auto">{icon}</Col> : <></>}
         <Col>
-          <small className={styles.timestamp}>
-            <IconEdit/>&nbsp;<TimeAgo epoch={entry.modifiedEpoch}/>
-          </small>
-          <small className={styles.timestamp}>
-            <IconPublish/>&nbsp;<TimeAgo epoch={entry.publishedEpoch}/>
-          </small>
-        </Col>
-        <Col className="text-right">
-          <small className="text-muted">
-            {t((t) => t.posts.info.viewCountComplete, {count: entry.viewCount.toString()})}
-          </small>
+          <h5>
+            <InternalLink
+              href={link}
+              locale={lang}
+              content={title}
+            />
+          </h5>
+          <Row noGutters>
+            <Col xs="auto">
+              {renderPostBadge({entry})}&nbsp;
+            </Col>
+            <Col>
+              <small className={styles.timestamp}>
+                <IconEdit/>&nbsp;<TimeAgo epoch={entry.modifiedEpoch}/>
+              </small>
+              <small className={styles.timestamp}>
+                <IconPublish/>&nbsp;<TimeAgo epoch={entry.publishedEpoch}/>
+              </small>
+            </Col>
+            <Col className="text-right">
+              <small className="text-muted">
+                {t((t) => t.posts.info.viewCountComplete, {count: entry.viewCount.toString()})}
+              </small>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
